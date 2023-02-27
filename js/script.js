752347379f52407c399a705ef3cce54ed982a04e
Felix <salgadocanga@gmail.com>
2023-02-27 17:42:09 -0500

    Initial commit - Se agrego los archivos HTML, CSS y JavaScript para la calculadora de tablas de multiplicar. También se agrego la funcionalidad para limpiar los resultados al cambiar los valores de entrada

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/* ---- &copy ---- */
+const fecha = Date.now();
+const hoy = new Date(fecha);
+const year = hoy.getFullYear();
+
+let footer = document.getElementById('year');
+footer.innerHTML = year;
+
+function mostrar() {
+    let table = document.getElementById('table').value;
+    let row = document.getElementById('row').value;
+    let respuesta = document.getElementById('respuesta');
+
+    if (table >= 0 && table <= 20) {
+        for (let i = 1; i <= row; i++) {
+            const resultado = document.createElement('p');
+            resultado.innerHTML = table + ' X ' + i + ' = ' + table * i;
+            respuesta.appendChild(resultado);
+        }
+        limpiarcampos();
+    }
+}
+
+function limpiar() {
+    let respuesta = document.getElementById('respuesta');
+    respuesta.innerHTML = '<p></p>';
+}
+
+function limpiarCampos() {
+    document.getElementById('table').value = '';
+    document.getElementById('row').value = '';
+}
